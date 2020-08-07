db000e21332a440f92011760af3cbb883199fbd4
Garrett Anstreicher <garrett.anstreicher@gmail.com>
2020-08-07 18:34:00 -0500

    Create tso_readin.jl

---
+++ Model/tso_readin.jl
@@ -0,0 +1,16 @@
+##readin targeted moments or auxiliary models
+function Readin_moments(dir::String)
+
+    #collect and return
+    package_moments = []
+    package_moments
+end
+
+
+#####read in model utilities (useful parameters or data estimated outside model)
+function Readin_utilities(dir::String)
+
+    #collect and return
+    package = []
+    package
+end
