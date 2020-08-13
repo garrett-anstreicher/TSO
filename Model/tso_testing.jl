fa3a0809e6e57103023398ff77ccc3a4b5a01c87
Garrett Anstreicher <garrett.anstreicher@gmail.com>
2020-08-13 17:41:51 -0500

    Update tso_testing.jl
    Debugged

--- Model/tso_testing.jl
+++ Model/tso_testing.jl
@@ -6,23 +6,16 @@ using Optim: optimize
 dir = "C:\\Users\\Garrett\\Documents\\Grad_School\\Papers\\TSO\\Model"
 cd(dir)
 
-include("tso_structs.jl")
 include("tso_readin.jl")
+include("tso_structs.jl")
 include("tso_utilities.jl")
 include("tso_background_functions.jl")
 include("tso_model.jl")
-include("tso_estimate.jl")
-include("tso_simulate.jl")
+#include("tso_simulate.jl")
+#include("tso_estimate.jl")
 
-#package = Readin_utilities(dir)
-#package_moments = Readin_moments(dir)
-
-
-
-
-
-
-
-
+#initialize parameters and primitives for the first time
+guess_init = Param_init(2, 3, 2, 2)
+@elapsed Solve_model(guess_init)
 
 ###########
