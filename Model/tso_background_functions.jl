f5b685eaeb8601f956076583616c7033f4d7a249
Garrett Anstreicher <garrett.anstreicher@gmail.com>
2020-08-13 17:41:37 -0500

    Update tso_background_functions.jl
    Debugged

--- Model/tso_background_functions.jl
+++ Model/tso_background_functions.jl
@@ -1,7 +1,7 @@
 ####File that contains functions governing wages, job offers, preferences, and unobserved heterogeneity
 
 #function that governs whether people receive teaching offers
-function μ(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, l::Int64)
+function μ(prim::Primitives, param::Params, Ω::Array{Any,1}, l::Int64)
     #arguments: license, last-period teaching choice, and total experience
     @unpack J, poly_exp = prim
     @unpack μ = param
@@ -24,24 +24,32 @@ function μ(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, l::Int
     if teach_last == 1 #taught last period
         prob += γ_l_int * l + sum(γ_exp_int.*exp_vec) #add interacted license/experience coefficients
     end
+
+    #bound
+    if prob>1.0
+        prob = 1.0
+    elseif prob<0.0
+        prob = 0.0
+    end
+
     prob #return deliverable
 end
 
 #occupation utility given parameters and current state
-function util(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, j::Int64, J::int64)
+function util(prim::Primitives, param::Params, Ω::Array{Any,1}, j::Int64, J::Int64)
 
     ###take care of some unpleasant setup
-    @unpack α, κ_j, γ_ju, ν_j, λ, σ_η = param
+    @unpack α, κ_j, γ_ju, ν, λ, σ_η = param
     ν_index = Ω[2][j+1] #location of taste state for occupation j -- in first go, will be 1 or 2
     X = Ω[1] #demographics
-    γ, κ, ν, σ_η = γ_ju[j], κ_j[j], ν_j[j], σ_η[j]
+    γ, κ, σ_η = γ_ju[j], κ_j[j], σ_η[j]
 
     #begin construction
-    util = α*w(prim, param, Ω, j) #start with wage
+    util = α* exp(w(prim, param, Ω, j)) #start with wage
     util += X[2] * γ[1] #gender utility component
     util += X[3] * γ[2] #race component
     util += κ * (j!=Ω[9]) #add job switching cost
-    util += ν[v_index] #add unobserved taste for occupation j
+    util += ν[ν_index] #add unobserved taste for occupation j
 
     #add teaching VA
     if j == J #in teaching
@@ -53,11 +61,11 @@ function util(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, j::I
 end
 
 #function that governs wage process
-function w(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, j::Int64)
+function w(prim::Primitives, param::Params, Ω::Array{Any,1}, j::Int64)
     @unpack J, nm, nm, poly_exp = prim
-    @unpack γ_ju, δ_j = param
+    @unpack γ_jw, δ_j = param
     X, θ, m, MA, l, exp = Ω[1], Ω[2][1], Ω[3], Ω[4], Ω[5], Ω[6][j] #relevant state variables
-    γ, δ = γ_ju[j], δ_j[j]
+    γ, δ = γ_jw[j], δ_j[j]
     wage = θ * δ #initialize with ability component (note: automatically 0 if in teaching)
 
     #polynomial of experience
@@ -68,30 +76,30 @@ function w(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, j::Int6
 
     if j == J #in teaching
         #prepare parameters
-        γ_int, γ_exp, γ_ma = γ[1], γ[2:(2+poly_exp)-1], γ[2+γ_exp]
-        wage += (γ_int + γ_ma*MA + sum(γ_exp.*exp_vec) #add terms
+        γ_int, γ_exp, γ_ma = γ[1], γ[2:(2+poly_exp)-1], γ[2+poly_exp]
+        wage += (γ_int + γ_ma*MA + sum(γ_exp.*exp_vec)) #add terms
     elseif j!=J #not in teaching
-        γ_int, γ_exp, γ_cq = γ[1], γ[2:(2+poly_exp)-1], γ[(2+γ_exp)] #intercept, experience, cq
-        index = 2+ γ_exp + 1 #for brevity
+        γ_int, γ_exp, γ_cq = γ[1], γ[2:(2+poly_exp)-1], γ[(2+poly_exp)] #intercept, experience, cq
+        index = 2+ poly_exp + 1 #for brevity
         γ_s, γ_r, γ_m = γ[index], γ[index+1], γ[index+2] #sex, race, major
 
         wage += (γ_int + sum(γ_exp.*exp_vec)) #add terms
         wage += (γ_s * X[2] + γ_r * X[3] + γ_m * m) #add sex, race, teaching major effects
     end
-    wage #return
+    wage
 end
 
 #function that governs teaching-va
-function va(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}})
-    @unpack γ_0, δ_0, poly_exp = param
-    @unpack J, nm = prim
+function va(prim::Primitives, param::Params, Ω::Array{Any,1})
+    @unpack γ_0, δ_0 = param
+    @unpack J, nm, poly_exp = prim
     X, θ, m, MA, l, exp = Ω[1], Ω[2][1], Ω[3], Ω[4], Ω[5], Ω[6][J] #relevant state variables
     γ, δ = γ_0, δ_0
 
     #setup
     γ_int, γ_exp, γ_cq = γ[1], γ[2:(2+poly_exp-1)], γ[2+poly_exp]
     index = 2 + poly_exp + 1
-    γ_g, γ_r, γ_m, γ_ma, γ_l = γ[index], γ[index+1], γ[index+2], γ[index+3], γ[index+4]
+    γ_g, γ_r, γ_m, γ_MA, γ_l = γ[index], γ[index+1], γ[index+2], γ[index+3], γ[index+4]
 
     #polynomial of experience
     exp_vec = []
@@ -107,13 +115,14 @@ function va(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}})
 end
 
 #function that governs how costly it is to obtain a teaching masters, ignoring utliity shocks
-function cost_MA(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}})
-    @unpack γ_ma, δ_ma = param
+function cost_MA(prim::Primitives, param::Params, Ω::Array{Any,1})
+    @unpack γ_MA, δ_MA = param
     @unpack J = prim
     X, θ, m = Ω[1], Ω[2][1], Ω[3] #relevant state variables
-    γ, δ = γ_ma, δ_ma
+    γ, δ = γ_MA, δ_MA
 
     #setup
+    γ_int, γ_cq, γ_g, γ_r, γ_m = γ[1], γ[2], γ[3], γ[4], γ[5]
 
     #construct
     cost = δ * θ #initialize with ability effect
@@ -123,7 +132,7 @@ end
 
 
 #function that governs how costly it is to obtain a license, ignoring utliity shocks
-function cost_license(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}})
+function cost_license(prim::Primitives, param::Params, Ω::Array{Any,1})
     @unpack γ_l, δ_l = param
     @unpack J = prim
     X, θ, m, MA = Ω[1], Ω[2][1], Ω[3], Ω[4] #relevant state variables
@@ -134,26 +143,25 @@ function cost_license(prim::Primitives, param::Params, Ω::Vector{Vector{Float64
 
     #construct
     cost = δ * θ #initialize with ability effect
-    cost += (γ_int + γ_cq * X[1] + γ_g*X[2] + γ_r*X[3] + γ_ma*MA + γ_m*m)
+    cost += (γ_int + γ_cq * X[1] + γ_g*X[2] + γ_r*X[3] + γ_m*m + γ_MA*MA)
     cost
 end
 
 #function that governs how costly it is to obtain certain college majors
-function cost_major(prim::Primitives, param::Params, Ω::Vector{Vector{Float64}}, i_m::Int64)
-    @unpack γ_m, δ_m, ρ_m, ν_j = param
+function util_major(prim::Primitives, param::Params, Ω::Array{Array{Int64,1},1}, i_m::Int64)
+    @unpack γ_m, δ_m, ρ_m, ν = param
     @unpack J = prim  #not quite so simple with teh ν
 
     X, θ = Ω[1], Ω[2][1] #relevant state variables
     ν_index = Ω[2][i_m + 1] #index of taste state for occupation j -- in first go, will be 1 or 2
-    ν_m = ν_j[i_m] #unobserved types for occupation corresponding to major
-    ν = ν_m[ν_index] #unobserved preference for the individaul, based on their state
+    ν_m = ν[ν_index] #unobserved preference for the individaul, based on their state
     γ, δ, ρ = γ_m[i_m], δ_m[i_m], ρ_m[i_m]
 
 
     γ_int, γ_cq, γ_g, γ_r = γ[1], γ[2], γ[3], γ[4]
 
     #construct
-    util = δ*θ + ρ*ν #initialize with unobserved effects
+    util = δ*θ + ρ*ν_m #initialize with unobserved effects
     util += γ_int + γ_cq*X[1] + γ_g * X[2] + γ_r * X[3] #intercpet, gender, race\
     util
 end
